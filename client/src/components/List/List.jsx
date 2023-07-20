e1a7ff7e44ac6b080d299ed6884d9f91faa56197
junaiyetmia470@gmail.com <rezowan.7890@gmail.com>
2023-07-20 17:58:33 +0600

    code update

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -1,51 +1,9 @@
 import React from "react";
 import "./List.scss";
 import Card from "../Card/Card";
+import useFetch from "../../components/hooks/useFetch.jsx";
 
-export default function List() {
-  const data = [
-    {
-      id: 1,
-      img: "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      img2: "https://images.pexels.com/photos/341523/pexels-photo-341523.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: " grapic ",
-      isNew: true,
-      oldPrice: 19,
-      price: 17,
-    },
-    {
-      id: 2,
-      img: "https://images.pexels.com/photos/341523/pexels-photo-341523.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      img2: "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Long sleve grapic asdf",
-      isNew: true,
-      oldPrice: 10,
-      price: 15,
-    },
-    {
-      id: 3,
-      img: "https://images.pexels.com/photos/1667088/pexels-photo-1667088.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      img2: "https://images.pexels.com/photos/3270223/pexels-photo-3270223.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Somers",
-      isNew: false,
-      oldPrice: 30,
-      price: 25,
-    },
-    {
-      id: 4,
-      img: "https://images.pexels.com/photos/3270223/pexels-photo-3270223.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      img2: "https://images.pexels.com/photos/1667088/pexels-photo-1667088.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Ioasdf",
-      isNew: true,
-      oldPrice: 12,
-      price: 19,
-    },
-  ];
-  return (
-    <div className="list">
-      {/* {data?.map((item) => (
-        <Card item={item} key={item.id} />
-      ))} */}
-    </div>
-  );
+export default function List({ catId, maxPrice, sort, subCategory }) {
+  const { data, loading, error } = useFetch(`products?populate=*&[filters][categories][id]=${catId}${subCategory.map((item) => `&[filters][sub_categories][id][$eq]=${item}`)}`);
+  return <div className="list">{loading ? "Loading...." : data?.map((item) => <Card item={item} key={item.id} />)}</div>;
 }
--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -2,38 +2,46 @@ import React, { useState } from "react";
 import "./Products.scss";
 import List from "../../components/List/List";
 import { useParams } from "react-router";
-
+import useFetch from "../../components/hooks/useFetch.jsx";
 export default function Products() {
-  const catId = parseInt(useParams().id);
+  const catId = useParams().id;
   const [maxPrice, setMaxPrice] = useState(1000);
   const [sort, setSort] = useState(null);
+  const [selectedSubCategory, setSelectedSubCategory] = useState([]);
+
+  const { data, loading, error } = useFetch(`/sub-categories?[filters][categories][id][$eq]=${catId}`);
+  console.log(data);
+
+  let handleChange = (e) => {
+    const value = e.target.value;
+    const isChecked = e.target.checked;
+
+    setSelectedSubCategory(isChecked ? [...selectedSubCategory, value] : selectedSubCategory.filter((item) => item !== value));
+  };
+
+  console.log(selectedSubCategory);
+
   return (
     <div className="products">
       <div className="left">
         <div className="filterItem">
           <h2>Product Categories</h2>
-          <div className="inptItem">
-            <input type="checkbox" id="1" value={1} />
-            <label htmlFor="1">Shoes</label>
-          </div>
-          <div className="inptItem">
-            <input type="checkbox" id="2" value={2} />
-            <label htmlFor="2">Skirts</label>
-          </div>
-          <div className="inptItem">
-            <input type="checkbox" id="3" value={3} />
-            <label htmlFor="3">Coats</label>
-          </div>
+          {data.map((item) => (
+            <div className="inptItem" key={item.id}>
+              {/* <input type="checkbox" id={item.id} value={item.id} onChange={handleChange} /> */}
+              <label htmlFor={item.id}>{item.attributes.title}</label>
+            </div>
+          ))}
         </div>
-        <div className="filterItem">
+        {/* <div className="filterItem">
           <h2>Filter by price</h2>
           <div className="inptItem">
             <span>0</span>
             <input type="range" min={0} max={1000} onChange={(e) => setMaxPrice(e.target.value)} />
             <span>{maxPrice}</span>
           </div>
-        </div>
-        <div className="filterItem">
+        </div> */}
+        {/* <div className="filterItem">
           <h2>Sort by</h2>
           <div className="inptItem">
             <input type="radio" id="asc" value="asc" name="price" onChange={(e) => setSort("asc")} />
@@ -43,11 +51,11 @@ export default function Products() {
             <input type="radio" id="desc" value="desc" name="price" onChange={(e) => setSort("desc")} />
             <label htmlFor="desc">Price (Highest first)</label>
           </div>
-        </div>
+        </div> */}
       </div>
       <div className="right">
         <img className="catImg" src="https://images.pexels.com/photos/5632402/pexels-photo-5632402.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
-        <List catId={catId} maxPrice={maxPrice} sort={sort} />
+        <List catId={catId} maxPrice={maxPrice} sort={sort} subCategory={selectedSubCategory} />
       </div>
     </div>
   );
